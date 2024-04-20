84ef1d51b25c72088a11a72c70f5fbe57ecb4066
Jefferson Oliveira <jeffersonmourak@gmail.com>
2024-04-20 12:24:13 -0300

    Implementing read for font table maps

--- Package.swift
+++ Package.swift
@@ -5,10 +5,17 @@ import PackageDescription
 
 let package = Package(
     name: "FontRenderer",
+    dependencies: [
+        .package(path: "../FontLoader"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "FontRenderer"),
+            name: "FontRenderer", dependencies: [
+                "FontLoader",
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            ]),
     ]
 )
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,4 +1,40 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+import ArgumentParser
+import FontLoader
 
-print("Hello, world!")
+func readFile(fromPath path: String) -> Data? {
+    let fileURL = URL(fileURLWithPath: path)
+
+    do {
+        let data = try Data(contentsOf: fileURL)
+        return data
+    } catch {
+        print("Error reading data: \(error)")
+        return nil
+    }
+}
+
+
+struct Random: ParsableCommand {
+    @Argument() var fontPath: String
+
+    func run() {
+        
+        guard let fontData = readFile(fromPath: fontPath) else {
+            print("No File at \(fontPath)")
+            return
+        }
+        
+        let font = FontLoader(withData: fontData)
+        
+        print(font.glyf)
+        
+//        var data :NSData? = FileUtility.dataFromPath("data") as? NSData
+        
+//        let content = NSString(contentsOfFile: fontPath, encoding: )
+        
+        
+    }
+}
+
+Random.main()
