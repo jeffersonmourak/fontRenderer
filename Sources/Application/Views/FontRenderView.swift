c2c8f963204ca1d247dfe193d4441cb208591260
Jefferson Oliveira <jeffersonmourak@gmail.com>
2024-05-05 16:30:48 -0300

    Added Debug lines for SafeCanvas Implementation

--- Sources/Application/Views/FontRenderView.swift
+++ Sources/Application/Views/FontRenderView.swift
@@ -8,6 +8,7 @@
 import Foundation
 import SwiftUI
 import FontLoader
+import Combine
 
 enum CurrentGlyph {
     case missing
@@ -15,59 +16,110 @@ enum CurrentGlyph {
     case existing(SimpleGlyphTable)
 }
 
+enum GlyphRenderType: Identifiable {
+    var id: UUID {
+        return UUID()
+    }
+    
+    case space
+    case character(CharacterMapItem)
+    case glyph(Int)
+}
+
 struct FontRenderView : View {
     var loader: FontLoader
     
-    @State var selectedGlyph: Int = 0
     @State var glyphCount: Int
     @State var currentGlyph: CurrentGlyph
     @State var fontScale = 0.3
+    @State var inputText: String = ""
+    @State var showDefaultGlyph: Bool = false
     
-    init(_ loader: FontLoader, startingAt selectedGlyph: Int = 2) {
+    init(_ loader: FontLoader) {
         
         self.loader = loader
-        self.selectedGlyph = selectedGlyph
         glyphCount = loader.glyphs.count
-        let glyphData =  loader.glyphs[selectedGlyph]
+        let glyphData =  loader.glyphs[0]
         
         currentGlyph = glyphData != nil ? .existing(glyphData!) : .missing
     }
     
     @ViewBuilder
+    func RenderGlyph(withType type: GlyphRenderType) -> some View {
+        switch type {
+        case .space:
+            Rectangle().fill(.clear).frame(width: 500 * fontScale, height: 730 * fontScale)
+        case let .character(char):
+            RenderGlyphView(glyphData: loader.glyphs[char.glyphIndex], scale: fontScale)
+        case let .glyph(index):
+            RenderGlyphView(glyphData: loader.glyphs[index], scale: fontScale)
+        }
+    }
+    
+    @ViewBuilder
     func viewGlyph(for state: CurrentGlyph) -> some View {
-        switch state {
-        case let .error(message):
-            Text(message)
-        case let .existing(glyph):
-            RenderGlyphView(glyphData: glyph, scale: fontScale)
-        case .missing:
-            Text("Missing Glyph")
+        let initialChars: [GlyphRenderType] = showDefaultGlyph ? [.glyph(0)] : []
+        
+        let chars: [GlyphRenderType] = inputText.reduce(initialChars) { chars, char in
+            guard let charMapItem = loader.characters[char] else {
+                return chars
+            }
+            var newChars = chars
+            
+            if char == " " {
+                newChars.append(.space)
+                
+                return newChars
+            }
+            
+            newChars.append(.character(charMapItem))
+            
+            return newChars
+        }
+        
+        HStack {
+            ForEach(chars) { char in
+                RenderGlyph(withType: char)
+            }
+        }
+    }
+    
+    func limitText(_ upper: Int) {
+        if inputText.count > upper {
+            inputText = String(inputText.prefix(upper))
         }
     }
     
     var body: some View {
         VStack {
-            HStack {
-                Picker("Glyph Index", selection: $selectedGlyph) {
-                    ForEach(0..<glyphCount) { glyphIndex in
-                        Text("\(glyphIndex)")
-                    }
-                }.onChange(of: selectedGlyph) {
-                    let glyphData =  loader.glyphs[selectedGlyph]
-                    
-                    currentGlyph = glyphData != nil ? .existing(glyphData!) : .missing
-                }
-                Slider(
-                    value: $fontScale,
-                    in: 0.1...1
-                    
-                )
-                Text("\(fontScale)")
-            }.frame(height: 30)
             HStack(alignment: .center){
-                viewGlyph(for: currentGlyph)
-            }.padding().frame(width: 640, height: 400)
-            
+                ScrollView(.horizontal) {
+                    viewGlyph(for: currentGlyph).padding()
+                }
+            }.frame(maxHeight: .infinity)
+            VStack {
+                HStack {
+                    Toggle(isOn: $showDefaultGlyph) {
+                        Text("Show Default Glyph")
+                    }
+                    .toggleStyle(.checkbox)
+                    Text("Font Scale \(fontScale)")
+                    Slider(
+                        value: $fontScale,
+                        in: 0.1...1
+                        
+                    )
+                }.padding()
+                
+                TextEditor(text: $inputText)
+                    .lineLimit(3, reservesSpace: true)
+                    .font(.system(.body))
+                    .frame(height: 60)
+                    .onReceive(Just(inputText)) { _ in limitText(100) }
+                
+            }
+            .frame(height: 90)
+            .padding()
         }
     }
 }
@@ -103,9 +155,15 @@ struct RenderGlyphView: View {
         
         var coords: [CGPoint] = []
         
+//        print(glyph)
+        
         for i in 0..<glyph.xCoordinates.count {
             let x = toScale(glyph.xCoordinates[i])
-            let y = toScale(glyph.yCoordinates[i])
+            
+            let rawY = (730 - glyph.yCoordinates[i])
+            
+            let y = toScale(rawY)
+            
             coords.append(CGPoint(x: x, y: y))
         }
         
@@ -129,7 +187,10 @@ struct RenderGlyphView: View {
     ]
     
     var body: some View {
+        let width = (Int(glyphData?.xMax ?? 0) - Int(glyphData?.xMin ?? 0)) + 100
+        
         Canvas { context, size in
+            
             guard let glyph = glyphData else {
                 return
             }
@@ -137,29 +198,64 @@ struct RenderGlyphView: View {
             var nextSegmentIndex = 0
             var beginOfContour: Int = 0
             
+//            print(glyph)
+            
+            let minPoints = CGPoint(x: Int(glyph.xMin), y: Int(glyph.yMin))
+            let maxPoints = CGPoint(x: Int(glyph.xMax), y: Int(glyph.yMax))
+            
+            if coords.count == 0 {
+                return;
+            }
+            
+            let _ = SafeCanvas(withBoundary: .init(a: minPoints, b:maxPoints))
+            
             var path = Path()
+            path.move(to: .init(x: Int(glyph.xMin), y: Int(glyph.yMin)))
+            path.addLines([
+                .init(x: 0, y: 0),
+                .init(x: size.width, y: .zero),
+                .init(x: size.width, y: size.height),
+                .init(x: .zero, y: size.height),
+                .init(x: 0, y: 0)
+                ]
+            )
+            
+            context.stroke(path, with: .color(.purple), lineWidth: 5)
+            
+            path = Path()
+            path.move(to: .init(x: Int(glyph.xMin), y: Int(glyph.yMin)))
+            path.addLines([
+                .init(x: toScale(glyph.xMin), y: toScale(730 - Int(glyph.yMin))),
+                .init(x: toScale(glyph.xMax), y: toScale(730 - Int(glyph.yMin))),
+                .init(x: toScale(glyph.xMax), y: toScale(730 - Int(glyph.yMax))),
+                .init(x: toScale(glyph.xMin), y: toScale(730 - Int(glyph.yMax))),
+                .init(x: toScale(glyph.xMin), y: toScale(730 - Int(glyph.yMin)))
+                ]
+            )
+            
+            context.stroke(path, with: .color(.green), lineWidth: 5)
+            
+            path = Path()
             path.move(to: coords[0])
             
             var points: [CGPoint] = [coords[0]]
             for i in 1..<coords.count {
                 let current = coords[i]
-                let nextSegment = glyph.endPtsOfContours[nextSegmentIndex]
+                let nextSegment = nextSegmentIndex < glyph.endPtsOfContours.count ? glyph.endPtsOfContours[nextSegmentIndex] : glyph.endPtsOfContours.last!
                 
                 points.append(current)
-
+                
                 if (i == nextSegment) {
-                    
                     points.append(coords[beginOfContour])
-                    
                     path.addLines(points)
-                    context.stroke(path, with: .color(colors[nextSegmentIndex]), lineWidth: 2)
-                    
+                    context.stroke(path, with: .color(.gray), lineWidth: 2)
+//                    print(points)
                     points = []
                     beginOfContour = nextSegment + 1
                     nextSegmentIndex += 1
                 }
             }
-        }
+        }.frame(width: toScale(width))
     }
 }
 
@@ -179,4 +275,19 @@ extension CGPoint: AdditiveArithmetic {
     public static func + (lhs: CGPoint, rhs: Double) -> CGPoint {
         return CGPoint(x: lhs.x + rhs, y: lhs.y + rhs)
     }
+}
+
+extension StringProtocol {
+    subscript(offset: Int) -> Character { self[index(startIndex, offsetBy: offset)] }
+    subscript(range: Range<Int>) -> SubSequence {
+        let startIndex = index(self.startIndex, offsetBy: range.lowerBound)
+        return self[startIndex..<index(startIndex, offsetBy: range.count)]
+    }
+    subscript(range: ClosedRange<Int>) -> SubSequence {
+        let startIndex = index(self.startIndex, offsetBy: range.lowerBound)
+        return self[startIndex..<index(startIndex, offsetBy: range.count)]
+    }
+    subscript(range: PartialRangeFrom<Int>) -> SubSequence { self[index(startIndex, offsetBy: range.lowerBound)...] }
+    subscript(range: PartialRangeThrough<Int>) -> SubSequence { self[...index(startIndex, offsetBy: range.upperBound)] }
+    subscript(range: PartialRangeUpTo<Int>) -> SubSequence { self[..<index(startIndex, offsetBy: range.upperBound)] }
 }
---
+++ Sources/Layout/SafeCanvas.swift
@@ -0,0 +1,21 @@
+//
+//  SafeCanvas.swift
+//
+//
+//  Created by Jefferson Oliveira on 4/30/24.
+//
+
+import Foundation
+
+struct Vector4 {
+    let a: CGPoint
+    let b: CGPoint
+}
+
+class SafeCanvas {
+    init (withBoundary bounds: Vector4) {
+//        print(bounds)
+        
+//        Vector4()
+    }
+}
