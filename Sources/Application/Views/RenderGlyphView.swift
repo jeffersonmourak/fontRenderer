6e59a8012156f3ff2105eb42234be5b012b71a80
Jefferson Oliveira <jeffersonmourak@gmail.com>
2024-05-10 10:10:36 -0300

    Added support for Generic Glyph rendering.

---
+++ Sources/Application/Views/RenderGlyphView.swift
@@ -0,0 +1,79 @@
+//
+//  RenderGlyphView.swift
+//
+//
+//  Created by Jefferson Oliveira on 5/9/24.
+//
+
+import Foundation
+import SwiftUI
+import FontLoader
+
+struct RenderGlyphView: View {
+    var glyph: Glyph
+    var scale: Double = 1
+    var fontHeight: Double
+    
+    var path = Path()
+    
+    func toScale<T: BinaryInteger>(_ value: T) -> CGFloat {
+        return CGFloat(Int((CGFloat(value) * CGFloat(scale)) * 1000) / 1000)
+    }
+    
+    func toScale(_ value: CGFloat) -> CGFloat {
+        return CGFloat((value * CGFloat(scale)) * 1000 / 1000)
+    }
+    
+    let colors: [Color] = [
+        .red,
+        .green,
+        .blue,
+        .yellow,
+        .purple,
+        .brown,
+        .cyan,
+        .orange,
+        .gray,
+    ]
+    
+    
+    
+    var body: some View {
+        let width = glyph.glyphBox.xMax > Double(glyph.fontLayout.horizontalMetrics.advanceWidth) ? CGFloat(glyph.glyphBox.xMax) : CGFloat(glyph.fontLayout.horizontalMetrics.advanceWidth)
+        
+        Canvas { context, size in
+            // @TODO: Find what to do with SafeCanvas
+            //let _ = SafeCanvas(withBoundary: .init(a: minPoints, b:maxPoints))
+            
+            var path = Path()
+            path.addLines([
+                .init(x: 0, y: 0),
+                .init(x: size.width, y: .zero),
+                .init(x: size.width, y: size.height),
+                .init(x: .zero, y: size.height),
+                .init(x: 0, y: 0)
+                ]
+            )
+            context.stroke(path, with: .color(.purple), lineWidth: 4)
+            
+            for glyphContours in glyph.contours {
+                let coords = glyphContours.map {
+                    let newY = ((glyph.fontLayout.fontBoundaries.1.y * 0.7) - $0.y) - Double(glyph.baseLineDistance)
+                    let newX = $0.x
+                    
+                    return CGPoint(x: toScale(newX), y: toScale(newY))
+                }
+     
+                if coords.count == 0 {
+                    return;
+                }
+                
+                path = Path()
+                path.move(to: coords[0])
+                path.addLines(coords)
+                context.stroke(path, with: .color(.gray), lineWidth: 2)
+            }
+            
+        }.frame(width: toScale(width), height: toScale(glyph.fontLayout.fontBoundaries.1.y * 0.7))
+    }
+}
